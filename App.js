65d26315d98c8c7bceee749994f07ed982bcc4b5
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-12 11:04:48 +0700

    Router 2

--- App.js
+++ App.js
@@ -2,19 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Weather from './components/Weather';
-import  Constants  from 'expo-constants';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110"/>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight
-  },
-});
+
