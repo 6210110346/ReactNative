af111e27b16a942e5ff9a8b32bb3a0200ff7ef18
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-18 18:56:30 +0700

    change background color

--- components/Weather.js
+++ components/Weather.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgba(0,0,0,0.5)',
         width: '100%',
-        height: '60%',
+        height: '40%',
     },
     white: {
         color: 'white'
