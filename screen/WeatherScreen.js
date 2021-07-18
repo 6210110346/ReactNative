8d5d40fa56b8ccbf6d2a119264ae5ae48020a28c
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-18 18:45:51 +0700

    change zipcode is -> place

--- screen/WeatherScreen.js
+++ screen/WeatherScreen.js
@@ -3,11 +3,9 @@ import { View } from 'react-native'
 import Weather from '../components/Weather'
 
 export default function WeatherScreen({route}){
-    console.log(route.params.zipPlace)
-    console.log(route.params.zipCode)
     return(
         <View>
-            <Weather zipCode = {route.params.zipCode} place = {route.params.place}/>
+            <Weather zipCode = {route.params.zipCode} place = {route.params.zipPlace}/>
         </View>
     )
 }
